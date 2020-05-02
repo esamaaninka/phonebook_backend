5e467e0bf54113b3680ea2281fb51b38297df39d
esamaaninka <61664607+esamaaninka@users.noreply.github.com>
2020-05-02 19:21:00 +0300

    3.1 step 1

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log('Hello world')
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const persons = [
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    },
+    {
+        name: "Arto Hellas",
+        number: "050-123456",
+        id: 11
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 12
+    }
+]
+
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const person = persons.find(p => {
+        //console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+        return p.id === id
+    })
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  
+  })
+//console.log('Hello world', persons)
+
+const port = 3003
+app.listen(port)
+console.log(`Server running on port ${port}`)
