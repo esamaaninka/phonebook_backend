fc08716174a576341290e78f6395bebc471ac04f
esamaaninka <61664607+esamaaninka@users.noreply.github.com>
2020-05-07 09:07:17 +0300

    changed port allocation to env or pre-defined 3001

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
   })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
