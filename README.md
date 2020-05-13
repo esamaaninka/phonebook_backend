540174faf165085e92106e2ced66f6c2467d41e2
esamaaninka <61664607+esamaaninka@users.noreply.github.com>
2020-05-13 09:58:14 +0300

    3.12 mongoDB usage

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Backend for phonebook app, Fullstack2020 excercise 3.x
 Local server running on port 3001 by default
 
 Heroku:
-https://safe-lake-35420.herokuapp.com/api/persons/
+https://powerful-mountain-66311.herokuapp.com/
+
 
 Note! if creating, editing Procfile in Windows, pay special attention to extra chars/symbols whatever added by windows - could not get it working despite trying to force UTF-8, created the file in linux computer and pushed to git. 
 
---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+// print process.argv
+/*process.argv.forEach((val, index) => {
+    console.log(`Argv ${index}: ${val}`);
+  });
+*/
+
+if ( process.argv.length<3 ) {
+    console.log('\nTo add a new name - give password, new name and number as arguments\n')
+    console.log('To list the phonebook content - give password as only argument\n')
+    process.exit(1)
+}
+
+const url =
+  `mongodb+srv://esa-fullstack:${process.argv[2]}@cluster0-fhhrm.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Phonebook = mongoose.model('Phonebook', phonebookSchema)
+
+const phonebook = new Phonebook({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+
+switch (process.argv.length) {
+
+    case 3: 
+        Phonebook.find({}).then(result => {
+            console.log('Phonebook:')
+            result.forEach(p => {
+              console.log(p.name + ' ' + p.number  )
+            })
+            mongoose.connection.close()
+          })
+        break;
+        
+    case 5:
+        phonebook.save().then(response => {
+          //console.log('phonebook saved!', response)
+          console.log(`Added ${response.name} number ${response.number} to phonebook`)
+          mongoose.connection.close()
+        })
+        break;
+}
+
